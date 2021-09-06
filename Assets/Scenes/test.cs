d1962d4e9ec0b38032d8586b7999f7b8a781ae9c
Auron <52473536+WigglyGull@users.noreply.github.com>
2021-09-07 11:23:34 +1200

    Testing git

---
+++ Assets/Scenes/test.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour{
+    void Start(){
+        Debug.Log("Auron is connected");
+    }
+
+    void Update(){
+        
+    }
+}
